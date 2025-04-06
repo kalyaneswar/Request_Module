53ed3e4d7a3750c31bb8c331d7853946e7f1d33b
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-04-06 23:21:02 +0530

    python

--- 06-http_authentication.py
+++ 06-http_authentication.py
@@ -1,4 +1,7 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
-
-r = requests.get("https://httpbin.org/basic-auth/user/passwd")
+r = requests.get("https://httpbin.org/basic-auth/user/passwd", auth=HTTPBasicAuth('username', 'password'))
+print(r)
+r = requests.get("https://httpbin.org/basic-auth/user/passwd", auth=HTTPBasicAuth('user', 'passwd'))
+print(r)
