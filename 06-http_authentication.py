1121e2183d030ac35b25f2f764dc20ad3bed7c0c
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-04-06 23:35:41 +0530

    python

--- 06-http_authentication.py
+++ 06-http_authentication.py
@@ -5,3 +5,6 @@ r = requests.get("https://httpbin.org/basic-auth/user/passwd", auth=HTTPBasicAut
 print(r)
 r = requests.get("https://httpbin.org/basic-auth/user/passwd", auth=HTTPBasicAuth('user', 'passwd'))
 print(r)
+
+r = requests.get("https://httpbin.org/basic-auth/kalyan/secret", auth=HTTPBasicAuth('kalyan', 'secret'))
+print(r)
